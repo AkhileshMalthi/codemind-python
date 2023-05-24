f937c006eabe514f0b3208664681f048a267348e
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-05-24 21:51:40 +0530

    n = int(input()) m = list(map(int,str(n))) s = 0 for i in range(1,len(m)+1):     s += m[i-1]**i print(True if s == n else False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,5 +1,6 @@
-n  = input()
-l = []
-for i in range(len(n)):
-    l.append(int(n[i])**(i+1))
-print(True if sum(l)==int(n) else False)
+n = int(input())
+m = list(map(int,str(n)))
+s = 0
+for i in range(1,len(m)+1):
+    s += m[i-1]**i
+print(True if s == n else False)
