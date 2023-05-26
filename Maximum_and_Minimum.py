49fbd629e1607e8331ea9cdb40ac8648f9978559
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-05-26 12:22:37 +0530

    n = int(input()) l = list(map(int,input().split())) unq = [] sameascount = [] for i in l:     if i not in unq:         unq.append(i) for i in unq:     if l.count(i) == i:         sameascount.append(i) if len(sameascount) == 0:     print(-1) else :     print(min(sameascount),max(sameascount))

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,14 @@
+n = int(input())
+l = list(map(int,input().split()))
+unq = []
+sameascount = []
+for i in l:
+    if i not in unq:
+        unq.append(i)
+for i in unq:
+    if l.count(i) == i:
+        sameascount.append(i)
+if len(sameascount) == 0:
+    print(-1)
+else :
+    print(min(sameascount),max(sameascount))
