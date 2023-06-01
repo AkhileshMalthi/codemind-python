cc76a63ee90324b5a8087ffaa9698723f1f61c37
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-06-01 19:35:48 +0530

    def oddoreven(n):     return True if n%2==0 else False
    n = int(input())
    nums = list(map(int,input().split()))
    count = 0
    for i in range(1,n-1):
        if oddoreven(nums[i-1]) == True and oddoreven(nums[i+1]) == True:
            count+=1
    print(count)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,10 @@
+def oddoreven(n):
+    return True if n%2==0 else False
+
+n = int(input())
+nums = list(map(int,input().split()))
+count = 0
+for i in range(1,n-1):
+    if oddoreven(nums[i-1]) == True and oddoreven(nums[i+1]) == True:
+        count+=1
+print(count)
