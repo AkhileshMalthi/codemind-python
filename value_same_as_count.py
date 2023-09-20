884aaf1f864ba6d92e0c24b29a3d2cfe8a41ef30
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-09-20 22:14:05 +0530

    n = int(input()) nums = list(map(int,input().split())) count = 0 for num in set(nums):     if nums.count(num) == num:         count+=1 print(count)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,8 +1,7 @@
 n = int(input())
 nums = list(map(int,input().split()))
-new = list(set(nums))
 count = 0
-for i in range(len(new)):
-    if new[i] == nums.count(new[i]):
+for num in set(nums):
+    if nums.count(num) == num:
         count+=1
 print(count)
